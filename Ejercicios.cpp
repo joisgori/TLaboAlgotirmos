82c8f23c9d1e49b32b7ef6d252cdc1b01e170507
joisgori <37356528+joisgori@users.noreply.github.com>
2019-08-22 22:39:22 -0600

    Versión final
    He agregado un menú para escoger el ejercicio que desea ejecutar, sin embargo no es un menú en buckle infinito, habría que correr cada vez que desea ejecutar x problema

--- Ejercicios.cpp
+++ Ejercicios.cpp
@@ -1,50 +1,72 @@
 #include <iostream>
 using namespace std;
 
-void sumaNumeros(int i, int j);
+//Firmas de las funciones
+void sumaNumeros(int i, int j); //Para las matrices (Ejercicio 1)
+int potencia(int x, int n); //Para el segundo ejercicio
+int numMenor(int i); //Para el tercer ejercicio
 
-int potencia(int x, int n);
-
-//TAREA 1 DE LABO:
+//Variables globales para EJ 1:*******
 //DEFINO MIS MATRICES:
-	int MA[3][3] = {{1,2,1}, 
-					{1,2,1},
-					{1,1,1}};
-					
-	int MB[3][3] = {{1,2,1},
-					{1,2,1},
-					{1,1,1}};
-	int MC[3][3];
+int MA[3][3] = {{1,2,1}, 
+				        {1,2,1},
+				        {1,1,1}};	
+int MB[3][3] = {{1,2,1},
+                {1,2,1},
+                {1,1,1}};
+int MC[3][3];
+//PARA EL TERCER EJERCICIO*******
+int arreglo[6] = {12,6,21,3,10,-2};
+int temp = arreglo[0];
 
-int main(){
 
-	int i=0, j=0;
-	
-	sumaNumeros(i,j);
-	
-	//Ahora imprimo mi respuesta, la matrix C
-	for (int k=0 ; k < 3 ; k++){
-		for (int l=0 ; l < 3 ; l++){
-			cout << MC[k][l] << " ";
-		}
-		cout << endl;
-	}
-	
-	//PARA EL SEGUNDO EJERCICIO
-	int x, n;
-	
-	cout << "Ingrese los valores de la base y su exponente: " << endl;
-	cout << "base: ";
-	cin >> x;
-	cout << endl;
-	cout << "exponente: ";
-	cin >> n;
-	cout << potencia(x, n);
-	
-	return 0;
+int ejercicio;
+
+int main() {
+  cout << "A continuación INGRESAR EL NÚMERO del ejercicio que desea ejecutar: " << endl;
+  cout << "1. Suma de matrices de forma recursiva: " << endl;
+  cout << "2. Potenciación de un número de forma recursiva: " << endl;
+  cout << "3. Encontrar el número menor de forma recursiva: " << endl;
+  cin >> ejercicio;
+
+  switch(ejercicio){
+    case 1:{
+      int i=0, j=0;
+      sumaNumeros(i,j);
+      //Ahora imprimo mi respuesta, la matrix C
+      for (int k=0 ; k < 3 ; k++){
+        for (int l=0 ; l < 3 ; l++){
+          cout << MC[k][l] << " ";
+          }
+          cout << endl;
+          }
+      break;
+    }
+    case 2:{
+      int x, n;
+      cout << "Ingrese los valores de la base y su exponente: " << endl;
+      cout << "base: ";
+      cin >> x;
+      cout << "exponente: ";
+      cin >> n;
+      cout << "Por ende, la respuesta es: " << potencia(x, n);
+      break;
+    }
+    case 3:{
+      int i = 0;
+      cout << "El número menor en el presente arreglo es: " << endl; 
+      cout << numMenor(i);
+      break;
+    }
+    default:{
+      cout << "Ingrese una opción válida";
+      break;
+    }
+  }
 }
 
-//Lógica de la función
+//Lógica de las funciones
+//1.
 void sumaNumeros(int i, int j){
 	if(i < 3 && j < 3){
 		MC[i][j] = MA[i][j] + MB[i][j];
@@ -57,7 +79,7 @@ void sumaNumeros(int i, int j){
 		sumaNumeros(i,j);
 	}
 }
-
+//2.
 int potencia(int x, int n){
 	if(n>0){
 		return x * potencia(x, n-1);
@@ -66,4 +88,15 @@ int potencia(int x, int n){
 		return 1;
 	}
 }
-
+//3.
+int numMenor(int i){ //con este iré recorriendo el arreglo...
+	if(i < sizeof(arreglo)/sizeof(arreglo[0])) { //condición de paro, cuando esté en la última posición del arreglo
+		if(temp > arreglo[i]){
+			temp = arreglo[i];
+		}else{
+			i++;
+		}
+		numMenor(i);
+	}
+	return temp;
+}
